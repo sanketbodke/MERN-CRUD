668e830fac78c9b83c10e2b0654ae06c603cc609
Shriyaaa.10 <114821672+shriyaExe@users.noreply.github.com>
2024-02-04 15:09:28 +0530

    Update user.route.js

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {createUser, getAllUsers, updateUser, deleteUser} from "../controllers/user.controller.js";
+import {createUser, getAllUsers, getUserById, updateUser, deleteUser} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/new").post(createUser)
 router.route("/").get(getAllUsers)
+router.route("/:id").get(getUserById)
 router.route("/:id/update").put(updateUser)
 router.route("/:id/delete").delete(deleteUser)
 
