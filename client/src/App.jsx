afeea7c29256799b868ff36fcf2248909d6c6b75
sanket bodke <sanketbodke26@gmail.com>
2024-02-04 22:56:42 +0530

    home page add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import user from "./pages/user.jsx";
-import updateUser from "./pages/updateUser.jsx";
+import user from "./pages/user/user.jsx";
+import updateUser from "./pages/user/updateUser.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
     return (
         <>
           <Router>
               <Routes>
-                  <Route path="/user" Component={user}></Route>
+                  <Route index Component={Home}></Route>
+                  <Route path="/users" Component={user}></Route>
                   <Route path="/user/:id/update" Component={updateUser}></Route>
               </Routes>
           </Router>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaNodeJs } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { DiMongodb } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { Link } from "react-router-dom";
+import "../App.css"
+import "../style/home.css"
+const Home = () => {
+    return (
+        <div className="homeContainer">
+            <div className="textContainer">
+                <h1>CRUD Application Using MERN Stack </h1>
+                <div className="iconsContainer">
+                    <DiMongodb/>
+                    <SiExpress/>
+                    <FaReact/>
+                    <FaNodeJs/>
+                </div>
+                <p>The MERN (MongoDB, Express.js, React, Node.js) stack is a popular choice for building full-stack web
+                    applications due to its versatility and efficiency. In this tutorial, we will walk through the
+                    process of creating a simple CRUD (Create, Read, Update, Delete) application using the MERN
+                    stack.</p>
+                <Link to="/users" className="btn">Let's Start</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
